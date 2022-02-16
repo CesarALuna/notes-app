d5cf9d60b5e212bb14816823088f66830fc74f9e
cesar <ceslun@gmail.com>
2022-02-15 20:23:29 -0800

    Init Sidebar component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
+import React from 'react'
+import Sidebar from './components/Sidebar'
 import Split from 'react-split'
 import { nanoid } from 'nanoid'
 import './App.css'
 
 function App() {
+  const [notes, setNotes] = React.useState([])
+  const [currentNoteId, setCurrentNoteId] = React.useState(
+    (notes[0] && notes[0].id) || ''
+  )
+
+  function createNewNote() {
+    const newNote = {
+      id: nanoid(),
+      body: "# Enter your markdown note's title here",
+    }
+    setNotes((prevNotes) => [newNote, ...prevNotes])
+    setCurrentNoteId(newNote.id)
+  }
+
+  function findCurrentNote() {
+    return (
+      notes.find((note) => {
+        return note.id === currentNoteId
+      }) || notes[0]
+    )
+  }
+
   return (
-    <div className="App">
-      <h1>AppComponent</h1>
-    </div>
+    <main>
+      {/* <Split sizes={[30, 70]} direction="horizontal" className="split"> </Split>*/}
+
+      <Sidebar
+        notes={notes}
+        currentNote={findCurrentNote()}
+        setCurrentNoteId={setCurrentNoteId}
+        newNote={createNewNote}
+      />
+      {currentNoteId && notes.length > 0}
+    </main>
   )
 }
 
---
+++ src/components/Sidebar.js
@@ -0,0 +1,10 @@
+export default function Sidebar(props) {
+  return (
+    <section className="pane sidebar">
+      <div className="sidebar--header">
+        <h3>Notes</h3>
+        <button className="new-note">+</button>
+      </div>
+    </section>
+  )
+}
