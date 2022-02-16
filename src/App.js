af26b762ca19159536dea5f34bd1e624e29ab294
cesar <ceslun@gmail.com>
2022-02-15 21:13:42 -0800

    Finish Editor & Sidebar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Sidebar from './components/Sidebar'
+import Editor from './components/Editor'
 import Split from 'react-split'
 import { nanoid } from 'nanoid'
 import './App.css'
@@ -19,6 +20,16 @@ function App() {
     setCurrentNoteId(newNote.id)
   }
 
+  function updateNote(text) {
+    setNotes((oldNotes) =>
+      oldNotes.map((oldNote) => {
+        return oldNote.id === currentNoteId
+          ? { ...oldNote, body: text }
+          : oldNote
+      })
+    )
+  }
+
   function findCurrentNote() {
     return (
       notes.find((note) => {
@@ -29,14 +40,26 @@ function App() {
 
   return (
     <main>
-      {/* <Split sizes={[30, 70]} direction="horizontal" className="split"> </Split>*/}
-
-      <Sidebar
-        notes={notes}
-        currentNote={findCurrentNote()}
-        setCurrentNoteId={setCurrentNoteId}
-        newNote={createNewNote}
-      />
+      {notes.length > 0 ? (
+        <Split sizes={[30, 70]} direction="horizontal" className="split">
+          <Sidebar
+            notes={notes}
+            currentNote={findCurrentNote()}
+            setCurrentNoteId={setCurrentNoteId}
+            newNote={createNewNote}
+          />
+          {currentNoteId && notes.length > 0 && (
+            <Editor currentNote={findCurrentNote()} updateNote={updateNote} />
+          )}
+        </Split>
+      ) : (
+        <div className="no-notes">
+          <h1>You have no notes</h1>
+          <button className="first-note" onClick={createNewNote}>
+            Create one now
+          </button>
+        </div>
+      )}
     </main>
   )
 }
