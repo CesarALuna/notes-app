f6f834855f8a72e0c1d4499660f1643735468d33
cesar <ceslun@gmail.com>
2022-02-16 17:31:11 -0800

    Put most recently changed note at top

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import './App.css'
 
 function App() {
   const [notes, setNotes] = React.useState(
-    JSON.parse(localStorage.getItem('notes')) || []
+    () => JSON.parse(localStorage.getItem('notes')) || []
   )
   const [currentNoteId, setCurrentNoteId] = React.useState(
     (notes[0] && notes[0].id) || ''
@@ -27,13 +27,18 @@ function App() {
   }
 
   function updateNote(text) {
-    setNotes((oldNotes) =>
-      oldNotes.map((oldNote) => {
-        return oldNote.id === currentNoteId
-          ? { ...oldNote, body: text }
-          : oldNote
-      })
-    )
+    setNotes((oldNotes) => {
+      const newArray = []
+      for (let i = 0; i < oldNotes.length; i++) {
+        const oldNote = oldNotes[i]
+        if (oldNote.id === currentNoteId) {
+          newArray.unshift({ ...oldNote, body: text })
+        } else {
+          newArray.push(oldNote)
+        }
+      }
+      return newArray
+    })
   }
 
   function findCurrentNote() {
